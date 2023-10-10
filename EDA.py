42fd4c9397760c383e52c7a2f4c42df04113c5bd
Vladislav Amza <vladamza63@gmail.com>
2023-10-10 22:41:46 +0300

    Exploratory Data Analysis (EDA.py)

---
+++ EDA.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 1. Load the dataset
+data = pd.read_csv('data\diabetes.csv')
+
+# 2. Overview of the dataset
+print(data.info())
+print(data.describe())
+
+# 3. Check for missing values
+print(data.isnull().sum())
+
+# 4. Visualize the distribution of the Outcome variable
+sns.countplot(data['Outcome'])
+plt.title('Distribution of Outcome (Diabetes)')
+plt.show()
+
+# 5. Distribution of continuous features
+continuous_features = ['Pregnancies', 'BMI', 'Insulin', 'Age']  # just a few for demonstration
+for feature in continuous_features:
+    plt.figure(figsize=(10,5))
+    sns.histplot(data[feature], kde=True)
+    plt.title(f'Distribution of {feature}')
+    plt.show()
+
+# 6. Correlation heatmap to see how features correlate with each other and with the target
+corr_matrix = data.corr()
+plt.figure(figsize=(10,8))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+plt.title('Correlation Heatmap')
+plt.show()
+
+# 7. Pairplot to visualize pairwise relationship and distribution
+sns.pairplot(data, hue='Outcome')
+plt.show()
